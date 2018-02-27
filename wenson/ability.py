a91f05512278cd72760c66721b25853ae518f242
Robert Placencia <placencia@WNCs-MacBook-Pro.local>
2018-02-26 17:49:04 -0700

    Changed python script to new directory

--- wenson/ability.py
+++ wenson/ability.py
@@ -1,11 +1,11 @@
 import os
 
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/sneakAttack")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/evasion")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/assasinate")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/uncannyDodge")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/cunningAction")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/lucky")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/Skulker")
-os.system("cat /Users/placencia/Documents/dnd/wenson/abillities/dualWield")
+os.system("cat ~/projects/dnd/wenson/abillities/sneakAttack")
+os.system("cat ~/projects/dnd/wenson/abillities/evasion")
+os.system("cat ~/projects/dnd/wenson/abillities/assasinate")
+os.system("cat ~/projects/dnd/wenson/abillities/uncannyDodge")
+os.system("cat ~/projects/dnd/wenson/abillities/cunningAction")
+os.system("cat ~/projects/dnd/wenson/abillities/lucky")
+os.system("cat ~/projects/dnd/wenson/abillities/Skulker")
+os.system("cat ~/projects/dnd/wenson/abillities/dualWield")
 
