ac92a3302478f152726d0a2afc3e599c674f0179
Robert Placencia <placencia@WNCs-MacBook-Pro.local>
2018-02-26 17:39:57 -0700

    Add files and scripts for my two characters

---
+++ edric/geary.py
@@ -0,0 +1,5 @@
+import os
+os.system("cat equipment")
+os.system("cat weapons")
+os.system("cat loot")
+os.system("cat groupLoot")
---
+++ wenson/character.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cat about")
+os.system("cat lvl")
+os.system("cat xp")
+os.system("cat hp")
+os.system("cat ac")
+os.system("cat speed")
+os.system("cat proficiency")
+os.system("cat initiative")
+os.system("cat stats")
+os.system("cat skills")
+os.system("cat saves")
